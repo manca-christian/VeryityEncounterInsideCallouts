77f9a4b8dbc32f74e81471afbefe009b04727829
GabryTm047 <gabriele.costa@itsincom.it>
2024-07-15 12:29:06 +0200

    miglioramento select

--- Angular/VerityEncounter/src/app/components/select-card/select-card.component.ts
+++ Angular/VerityEncounter/src/app/components/select-card/select-card.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-select-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './select-card.component.html',
-  styleUrl: './select-card.component.scss'
+  styleUrl: './select-card.component.scss',
 })
 export class SelectCardComponent {
+  selectedCard: number | undefined;
+  select(id: number) {
+    this.selectedCard = id;
+    console.log(id);
+  }
 
+  onOptionSelected(event: Event): void {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    console.log('Opzione selezionata:', selectedValue);
+    switch (selectedValue) {
+      case 'circle':
+        this.select(1);
+        break;
+      case 'square':
+        this.select(2);
+        break;
+      case 'triangle':
+        this.select(3);
+        break;
+      default:
+        console.log('Opzione non gestita:', selectedValue);
+        break;
+    }
+  }
 }
