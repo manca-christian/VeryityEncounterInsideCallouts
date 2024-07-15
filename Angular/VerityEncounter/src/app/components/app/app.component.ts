144bba06e109888bdc0b08e2c5cedb1801fda896
GabryTm047 <gabriele.costa@itsincom.it>
2024-07-15 11:00:37 +0200

    immagine

--- Angular/VerityEncounter/src/app/components/app/app.component.ts
+++ Angular/VerityEncounter/src/app/components/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SelectCardComponent } from '../select-card/select-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,SelectCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Angular/VerityEncounter/src/app/components/select-card/select-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './select-card.component.html',
+  styleUrl: './select-card.component.scss'
+})
+export class SelectCardComponent {
+
+}
