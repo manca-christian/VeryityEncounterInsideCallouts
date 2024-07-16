d1ed9f5a943ace8e3aafcc79cd3b5149dfc5ecc8
GabryTm047 <gabriele.costa@itsincom.it>
2024-07-16 09:52:13 +0200

    binding

--- Angular/VerityEncounter/src/app/components/select-card/select-card.component.ts
+++ Angular/VerityEncounter/src/app/components/select-card/select-card.component.ts
@@ -11,13 +11,30 @@ import { Component } from '@angular/core';
 export class SelectCardComponent {
   selectedOption: string = '';
   selectedCard: number | undefined;
+  selectedShape: string | undefined;
 
   setOptionValue(option: string): void {
     this.selectedOption = option;
   }
   select(id: number) {
     this.selectedCard = id;
+    switch (id) {
+      case 1:
+        this.selectedShape = 'Circle';
+        break;
+      case 2:
+        this.selectedShape = 'Square';
+        break;
+      case 3:
+        this.selectedShape = 'Triangle';
+        break;
+      default:
+        console.log('Figura non gestita:', id);
+        break;
+    }
+    console.log(this.selectedShape);
   }
+
   onOptionSelected(event: Event): void {
     const selectedValue = (event.target as HTMLSelectElement).value;
     console.log('Opzione selezionata:', selectedValue);
